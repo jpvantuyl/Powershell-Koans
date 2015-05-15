fd5b2ea3dd40eac6f96e5f5d3675df000ec3c2ca
John Van Tuyl <jpvantuyl@gmail.com>
2015-05-15 13:52:23 -0700

    strings are strings

--- src/koans/02AboutStrings.Tests.ps1
+++ src/koans/02AboutStrings.Tests.ps1
@@ -1,9 +1,9 @@
 Describe "AboutStrings" {
 	It "should be fine with double quotes" {
-		$__FILL_ME_IN__ | Should Be "Hello, world.".GetType().ToString()
+		"system.string" | Should Be "Hello, world.".GetType().ToString()
 	}
 	
 	It "should be fine with single quotes" {
-		$__FILL_ME_IN__ | Should Be 'Hello, world.'.GetType().ToString()
+		"system.string" | Should Be 'Hello, world.'.GetType().ToString()
 	}
 }
