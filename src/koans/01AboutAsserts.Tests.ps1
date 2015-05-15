3a8c69ea0027ac6a0ca2b73cb9673ca4235d75d1
John Van Tuyl <jpvantuyl@gmail.com>
2015-05-15 13:51:46 -0700

    assert asserts

--- src/koans/01AboutAsserts.Tests.ps1
+++ src/koans/01AboutAsserts.Tests.ps1
@@ -1,11 +1,11 @@
 Describe "AboutAsserts" {
 	It "should expect true" {
 		# We shall contemplate truth by testing reality, via asserts.
-		$false | Should Be $true # This should be true
+		$true | Should Be $true # This should be true
 	}
 	
 	It "should fill in values" {
 		# Sometimes we will ask you to fill in the values
-		$__FILL_ME_IN__ | Should Be (1 + 1)
+		2 | Should Be (1 + 1)
 	}
 }
