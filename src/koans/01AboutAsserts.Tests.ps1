bc17522c0b678ff04d0b73eccd4812c0c1e4f64f
John Van Tuyl <jpvantuyl@gmail.com>
2015-05-18 18:59:57 -0700

    added expected and actual

--- src/koans/01AboutAsserts.Tests.ps1
+++ src/koans/01AboutAsserts.Tests.ps1
@@ -8,4 +8,11 @@ Describe "AboutAsserts" {
 		# Sometimes we will ask you to fill in the values
 		2 | Should Be (1 + 1)
 	}
+	
+	It "should assert one's expectations" {
+		# Some ways of asserting equality are better than others
+		$expected = 2 #$__FILL_ME_IN__
+		$actual = 1 + 1
+		$expected | Should Be $actual
+	}
 }
