458ffcdc52c057628dd480b2e29540fb1c03e95a
John Van Tuyl <jpvantuyl@gmail.com>
2015-08-05 15:31:14 -0700

    skeletons of control and loops

---
+++ src/koans/09AboutControlStatements.Tests.ps1
@@ -0,0 +1,12 @@
+#see Get-Help about_If
+#see Get-Help about_Switch
+Describe "AboutControlStatements" {
+	It "should support if statements" {	
+	}
+	It "should support if then statements" {	
+	}
+	It "should support if then else statements" {	
+	}
+	It "should support switch statements" {	
+	}
+}
---
+++ src/koans/10AboutLoops.Tests.ps1
@@ -0,0 +1,20 @@
+#see Get-Help about_For
+#see Get-Help about_Foreach
+#see Get-Help about_Do
+#see Get-Help about_While
+#see Get-Help about_Break
+#see Get-Help about_Continue
+Describe "AboutControlStatements" {
+	It "should support for statements" {	
+	}
+	It "should support foreach statements" {	
+	}
+	It "should support do statements" {	
+	}
+	It "should support while statements" {	
+	}
+	It "should support break statements" {	
+	}
+	It "should support continue statements" {	
+	}
+}
